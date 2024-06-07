5721dfe30821a5ea1c671fc65a33fcf9861b2350
peter <peteronojeruo@gmail.com>
2024-06-07 04:56:17 -0700

    changes made on the role and insight page

--- src/pages/Insight.jsx
+++ src/pages/Insight.jsx
@@ -14,75 +14,105 @@ const Insight =()=>{
 
             <div className="insight ">
 
-                <div class="accordion accordion-flush " id="accordionFlushExample">
-                    <div class="accordion-item ">
-                        <h2 class="accordion-header ">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Profile views
+                <div className="accordion accordion-flush" id="accordionFlushExample">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                        <button
+                            className="accordion-button" 
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseOne"
+                            aria-expanded="true" 
+                            aria-controls="flush-collapseOne"
+                        >
+                            Insight
                         </button>
                         </h2>
-                        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            <div className="d-flex justify-content-between ">
-                                    <div className="context">My views</div>
-                                    <div className="d-flex ">
-                                        <p className="me-2 mt-1" >45</p>
-                                        <div className="mt-1"><img src={ secImage} alt='views' className="eye" /></div>
+                        <div
+                        id="flush-collapseOne"
+                        className="accordion-collapse show" 
+                        data-bs-parent="#accordionFlushExample"
+                        >
+                        <div className="accordion-body">
+                            <div className="d-flex justify-content-between">
+                                <div className="context">
+                                    Profile visits
+                                </div>
+                                <div className="d-flex">
+                                    <p className="me-2 mt-1">45</p>
+                                    <div className="mt-1">
+                                    <img src={secImage} alt="views" className="eye" />
                                     </div>
-                                    </div>
+                                </div>
                             </div>
+                        </div>
                         </div>
                     </div>
 
-                    <div class="accordion-item mt-5">
-                        <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    <div className="accordion-item mt-4">
+                        <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseTwo"
+                            aria-expanded="false"
+                            aria-controls="flush-collapseTwo"
+                        >
                             Links
                         </button>
                         </h2>
-                        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body  ">
-                                
-                                
+                        <div
+                        id="flush-collapseTwo"
+                        className="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushExample"
+                        >
+                            <div className="accordion-body">
                                 <div className="d-flex justify-content-between cn">
-                                <div className="context">Facebook</div>
-                                <div className="d-flex ">
-                                    <p className="me-2 mt-1" >30</p>
-                                    <div className="mt-1"><img src={ secImage} alt='views' className="eye" /></div>
-                                </div>
-                                </div>
-                                
-                                <div className="d-flex justify-content-between cn">
-                                <div className="context">instagram</div>
-                                <div className="d-flex ">
-                                    <p className="me-2 mt-1" >10</p>
-                                    <div className="mt-1"><img src={ secImage} alt='views' className="eye" /></div>
-                                </div>
+                                    <div className="context">
+                                        Facebook
+                                    </div>
+                                    <div className="d-flex">
+                                        <p className="me-2 mt-1">30</p>
+                                        <div className="mt-1">
+                                            <img src={secImage} alt="views" className="eye" />
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <div className="d-flex justify-content-between cn">
-                                <div className="context">X(twitter)</div>
-                                <div className="d-flex ">
-                                    <p className="me-2 mt-1" >9</p>
-                                    <div className="mt-1"><img src={ secImage} alt='views' className="eye" /></div>
+                                    <div className="context">Instagram</div>
+                                    <div className="d-flex">
+                                        <p className="me-2 mt-1">10</p>
+                                        <div className="mt-1">
+                                            <img src={secImage} alt="views" className="eye" />
+                                        </div>
+                                    </div>
                                 </div>
+
+                                <div className="d-flex justify-content-between cn">
+                                    <div className="context">X (Twitter)</div>
+                                    <div className="d-flex">
+                                        <p className="me-2 mt-1">9</p>
+                                        <div className="mt-1">
+                                            <img src={secImage} alt="views" className="eye" />
+                                        </div>
+                                    </div>
                                 </div>
-                                
-                            
+
                                 <div className="d-flex justify-content-between">
-                                <div className="context">Linkdln</div>
-                                <div className="d-flex ">
-                                    <p className="me-2 mt-1" >25</p>
-                                    <div className="mt-1"><img src={ secImage} alt='views' className="eye" /></div>
+                                    <div className="context">LinkedIn</div>
+                                    <div className="d-flex">
+                                        <p className="me-2 mt-1">25</p>
+                                        <div className="mt-1">
+                                            <img src={secImage} alt="views" className="eye" />
+                                        </div>
+                                    </div>
                                 </div>
-                                </div>
-
                             </div>
                         </div>
                     </div>
-
-                            
-                </div>
+                </div> 
 
             </div>
 
